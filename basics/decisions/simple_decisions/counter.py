90da09d36fea720faed283a5ce5b8bb404239099
ToniAnton22 <72076515+ToniAnton22@users.noreply.github.com>
2020-10-06 15:14:29 +0300

    Update counter.py
    Added code to make variables into a counter.

--- basics/decisions/simple_decisions/counter.py
+++ basics/decisions/simple_decisions/counter.py
@@ -7,9 +7,25 @@ third_number=int(input("Insert third whole number "))
 print(third_number)
 
 
-
-if result1 + result2 == 0:
-  print("There is 1 even and 2 odd numbers")
-elif (first_number%2)*(second_number + third_number)%2 != 0:
-  print("There ")
-
+if first_number%2 != 0:
+  if (second_number* third_number)%2 != 0:
+    print("There are 3 odd numbers.")
+  elif second_number%2 == 0:
+    if third_number%2 != 0:
+      print("There are 1 even numbers and 2 odd one!")
+    elif third_number%2 == 0:
+      print("There are 2 even number and 1 odd number.")  
+  elif second_number%2 != 0:
+    if third_number%2 == 0:
+      print("There are 2 odd numbers and 1 even number.")  
+elif first_number%2 ==0:
+  if (second_number* third_number)%2 != 0:
+    print("There are 2 odd and 1 even numbers.")
+  elif second_number%2 == 0:
+    if third_number%2 != 0:
+      print("There are 2 even numbers and 1 odd one!")
+    elif third_number%2 == 0:
+      print("There are 3 even numbers.")  
+  elif second_number%2 != 0:
+    if third_number%2 == 0:
+      print("There are 1 odd numbers and 2 even number.")  
