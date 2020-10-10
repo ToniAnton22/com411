14134a28aa0325f6b9f0da0ebf4fb6e51e777b48
repl.it user <replituser@example.com>
2020-10-10 18:55:02 +0000

    Adding code to use an 'and' operator

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,14 @@
+# Trying out and more!
+
+print("What did I hear?")
+hear = input()
+print("What did I see?")
+sight = input()
+
+if hear == 'grrr' and sight == 'two red eyes':
+  print("There is a scary creature. I should get out of here!")
+# seems as if the "and" operator takes only takes the answers
+# only if they are both True
+# hmmmmmmmmmmmmmm
+else:
+  print("I am a little scared, but I will continue")
