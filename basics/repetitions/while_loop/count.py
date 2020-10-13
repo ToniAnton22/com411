e822f174fb450846260b3e04d8b59f0d55ff02ee
repl.it user <replituser@example.com>
2020-10-13 09:38:04 +0000

    Added code to demonstrate a while loop

--- basics/repetitions/while_loop/count.py
+++ basics/repetitions/while_loop/count.py
@@ -1,6 +1,10 @@
 # Demonstrating while loops
-cables = int(input("How many cables should I remove?"))
+cables = int(input("How many live cables should I avoid?"))
+liveCables = cables
+cables = 0
+while cables != liveCables :
+  print("Avoiding....")
+  cables = cables + 1
+  print("Done! {} live cables has been avoided".format(cables))
 
-while cables > 0 and cables <= 10:
-   print("Removing {} cables".format(cables))
-   cables = cables + 1
+print ("All live cables have been avoided.")
