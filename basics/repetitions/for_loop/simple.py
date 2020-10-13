d0bb1775fdc9b25f6fcb57282d1fa559f91c4d40
repl.it user <replituser@example.com>
2020-10-13 11:47:22 +0000

    Using for loops to print mountains

---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,12 @@
+
+mountain = int(input("How many mountains should I display? "))
+
+for count in range(0,mountain,1):
+  print("     __  ")  
+  print("    /  \_")  
+  print("   /^    \ ")
+  print("  /  ^    \_ ")
+  print("_/ ^ ^     ^\ ")
+  print("/  ^     ^    \ ")
+
+print("Done!")
