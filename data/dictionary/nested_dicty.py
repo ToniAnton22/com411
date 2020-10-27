8e84500a5df92b4fd5bd820ef767d946dd09199a
repl.it user <replituser@example.com>
2020-10-27 13:01:06 +0000

    Adding code to demonstrate usage of nested dictionaries

---
+++ data/dictionary/nested_dicty.py
@@ -0,0 +1,17 @@
+def short_pattern():
+  pattern = {"sequence":"101", "occurrence":5}
+  return pattern
+
+def medium_pattern():
+  pattern = {"sequence":"111000", "occurrences":25}
+  return pattern
+
+def long_pattern():
+  pattern = {"sequence":"1100110011001100","occurrences":50}
+  return pattern
+
+def run():
+  print("Analyizing patterns..")
+  patterns={"short sequence":short_pattern(),"medium sequence":medium_pattern(),"long sequence":long_pattern()}
+  print(patterns)
+run()
