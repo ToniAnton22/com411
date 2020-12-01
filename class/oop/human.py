e48f292d9831657be82131dc175ac313a347eb64
repl.it user <replituser@example.com>
2020-12-01 10:48:32 +0000

    Updated classes human and robot with instance methods

--- class/oop/human.py
+++ class/oop/human.py
@@ -13,7 +13,8 @@ class Robot:
     # An instance attribute
     self.name = "Robot"
     self.age = 0
-
+  def grow(self):
+    self.age += 1
   # An instance method
   def display(self):
     print(f"I am {self.name}")
@@ -25,6 +26,8 @@ class Robot:
 if (__name__ == "__main__"):
   robot = Robot()
   robot.display()
+  robot.grow()
+  robot.grow()
   print(robot)
 
 class Human:
@@ -34,13 +37,39 @@ class Human:
     self.name = "Human"
     self.age = 0
     self.energy= Human.MAX_ENERGY
+  
+  def grow(self):
+    self.age += 1
+  
+  def eat(self):
+    self.energy += 10
+    if(self.energy >= Human.MAX_ENERGY):
+      print("I'm full, I can no longer eat.")
+      self.energy = Human.MAX_ENERGY
+    else:
+      print("I feel energized")
+
+  
+  def move(self):
+    if(self.energy == 0):
+      print("You died")
+      self.energy = 0
+    else:
+      self.energy -= 10
+
   def display(self):
     print(f"I am {self.name}")
+
   def __str__(self):
-    return f'My name is {self.name} and I am {self.age} years old.'
+    return f'My name is {self.name} and I am {self.age} years old, and my energy is {self.energy}'
   
 
 if(__name__=="__main__"):
   human = Human()
   human.display()
+  human.eat()
+  human.grow()
+  human.move()
+  human.move()
+  human.eat()
   print(human)
